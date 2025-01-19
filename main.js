32fb889626859ceca35986c79d54a869e9424a83
danielcruz <danielcscruz@gmail.com>
2025-01-19 17:09:14 -0300

    fix: links and upload

--- main.js
+++ main.js
@@ -8,7 +8,18 @@ $(document).ready(function() {
 
     $('form').on('submit', function(e) {
         e.preventDefault();
-        const enderecoDaNovaImagem = $
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoDaNovaImagem}" alt="Imagem nova"/>`).appendTo(novoItem);
+        $(`
+            <div class="overlar-imagem-link">
+            <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+            Ver imagem em tamanho real
+            </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('');
     })
-
 })
